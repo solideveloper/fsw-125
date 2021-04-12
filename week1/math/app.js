1dd51de110a2004c5f66a052b2f5768a887e5b36
reposoli <stephanie.olivares@bryanuniversity.edu>
2021-04-12 14:04:50 -0400

    Node Module Basics & Patterns

---
+++ week1/math/app.js
@@ -0,0 +1,6 @@
+const doMath = require('./math');
+console.log(doMath);
+console.log(doMath.sum(10,2));
+console.log(doMath.subtract(10,2));
+console.log(doMath.divide(10,2));
+console.log(doMath.product(10,2));
---
+++ week1/math/math.js
@@ -0,0 +1,21 @@
+const sum = (a, b) => {
+     return a + b
+}
+
+const subtract = (c, d) => {
+     return c - d
+}
+
+const divide = (e, f) => {
+     return e / f
+}
+
+const product = (g, h) => {
+     return g * h
+}
+module.exports = {
+     sum,
+     subtract,
+     divide,
+     product
+};
