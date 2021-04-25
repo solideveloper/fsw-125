b400274b8cf5711951d5839d7d6a255e3085b77b
reposoli <stephanie.olivares@bryanuniversity.edu>
2021-04-24 23:16:33 -0400

    added screencast

--- week2/myapp/server.js
+++ week2/myapp/server.js
@@ -88,3 +88,5 @@ const songs = [
  app.listen(PORT, () => {
      console.log("The server running on Port: 3000")
  })
+
+ //I'm using a mac....  After npm install -g nodemon I was having permission issues as well as 'command not found' when trying to run 'nodeman server.js'. I added the script: 'dev": "npx nodemon server.js' in my package.json and I used npx nodeman server.js to run port 3000.  I used both these commands after googling and not sure I understand which of the two worked. When I try to run nodemon server.js I continually get command not found.. I'm sure it has to do with permission. but i dont know how to get TO those permissions. -need help understanding. 
