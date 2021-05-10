ee1de8893f6a9a3a2ba01427b5ba754d716acd14
reposoli <stephanie.olivares@bryanuniversity.edu>
2021-05-10 19:22:38 -0400

    test

--- week4/todoapp/server.js
+++ week4/todoapp/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan')
 
+const PORT = 7000;
 
 app.use(express.json());
 app.use(morgan('dev'))
 
 app.use("/todolist", require("./route/todoList.js"));
 
-app.listen(7000, () => {
-    console.log("Server running on Port 7000.")
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
 });
